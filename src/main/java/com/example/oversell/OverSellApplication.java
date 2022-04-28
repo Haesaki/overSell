54d82f17e3d81d437419251525d0721b9c1161e6
hasaeki <84838062+Haesaki@users.noreply.github.com>
2022-04-28 13:29:50 +0800

    Initial commit

---
+++ src/main/java/com/example/oversell/OverSellApplication.java
@@ -0,0 +1,11 @@
+package com.example.oversell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverSellApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OverSellApplication.class, args);
+    }
+}
